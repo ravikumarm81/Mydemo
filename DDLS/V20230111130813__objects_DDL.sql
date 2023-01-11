3ee112af15d64fab99fdf0f49dac0a761166f67f
ravikumarm81 <121169650+ravikumarm81@users.noreply.github.com>
2023-01-11 13:08:17 +0530

    committing files

---
+++ DDLS/V20230111130813__objects_DDL.sql
@@ -0,0 +1,7 @@
+-- test_tbl1
+CREATE   TABLE DEMO_127.DEMO.test_tbl1 (col1 varchar,
+col2 varchar) ;
+-- test_tbl2
+CREATE   TABLE DEMO_127.DEMO.test_tbl2 (col1 varchar,
+col2 varchar,
+col3 varchar) ;
